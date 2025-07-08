5a785a2ff1b1fba28adbdf1667105f459d224135
GoZiko <anirban89m@gmail.com>
2025-07-08 10:23:14 +0530

    Update lineage_violet.mk

--- lineage_violet.mk
+++ lineage_violet.mk
@@ -8,16 +8,7 @@
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
 # Inherit some common DerpFest AOSP stuff.
-# $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-DERPFEST_BUILD_TYPE := Official
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_NOT_USES_BLUR := true
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_SUPPORTS_CALL_RECORDING := true
-TARGET_INCLUDE_ACCORD ?= true
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 #Matrixx Os
 # To include Gapps 
