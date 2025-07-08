9c769ca1befcf633287101a1d6b2559d0ebb76f4
GoZiko <anirban89m@gmail.com>
2025-07-08 09:50:04 +0530

    Update device.mk Added Viper4Android

--- device.mk
+++ device.mk
@@ -11,6 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Added Viper4Android
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
